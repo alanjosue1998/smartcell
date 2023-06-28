36517edf66214713f264a698acd9ca6df96b645d
alanjosue1998 <josuerueda1998@gmail.com>
2023-06-27 22:21:42 -0500

    5: Add Get and Post.

--- src/routes/phones.routes.js
+++ src/routes/phones.routes.js
@@ -4,5 +4,6 @@ import { methods as phonesController } from "./../controllers/phones.controller"
 const router = Router();
 
 router.get("/", phonesController.getPhones);
+router.post("/", phonesController.addPhones);
 
 export default router;
