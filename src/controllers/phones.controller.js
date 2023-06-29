3f975c774603fd45e44dd620fb8dbb98ae831aaa
alanjosue1998 <josuerueda1998@gmail.com>
2023-06-28 22:08:50 -0500

    6: Add Put Delete and Post.

--- src/controllers/phones.controller.js
+++ src/controllers/phones.controller.js
@@ -10,8 +10,20 @@ const getPhones = async(req,res) => {
         res.status(500);
         res.send(error.message);
     }
+};
 
-}
+const getPhone = async(req,res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("Select id, brand, model, price, description, storage, ram, screen, camera, category, status from Phones WHERE id = ?", id);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 
 const addPhones = async(req,res) => {
     try {
@@ -29,10 +41,45 @@ const addPhones = async(req,res) => {
         res.status(500);
         res.send(error.message);
     }
+};
 
-}
+const deletePhone = async(req,res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM Phones WHERE id = ?", id);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const updatePhone = async(req,res) => {
+    try {
+        const { id } = req.params;
+        const { brand,model,price,description,storage,ram,screen,camera,category,status } = req.body;
+
+        if (id === undefined || brand === undefined || model == undefined ) {
+            res.status(400).json({ message: "Bad Request. Please fill all field."})
+        }
+
+        const phones = { brand,model,price,description,storage,ram,screen,camera,category,status };
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE Phones SET ? where id = ?", [phones, id]);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 
 export const methods = {
     getPhones,
+    getPhone,
+    deletePhone,
+    updatePhone,
     addPhones
 };
